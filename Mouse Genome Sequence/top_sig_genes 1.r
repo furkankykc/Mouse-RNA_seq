04d58f7eea291a8fa969e136464ba35def796739
Furkan kıyıkçı <furkanfbr@gmail.com>
2022-01-28 13:46:07 +0300

    Delete Mouse Genome Sequence directory

--- Mouse Genome Sequence/top_sig_genes 1.r
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env Rscript
-library(dplyr)
-load('../sig_genes.rda')
-top20 = arrange(sig_genes[1:20,],pval)
-top = arrange(sig_genes,pval)
-head(top20)
-write.table(top20, file = "top20_genes.tsv", row.names=FALSE, sep="\t")
-write.table(top, file = "top_genes.tsv", row.names=FALSE, sep="\t")
